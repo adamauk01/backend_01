64a34148dc3daeb911dedec885d3dadf2cb69d5f
vanh601 <vanh18601k@gmail.com>
2024-04-30 22:13:15 +0700

    connection pool

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,12 @@
-
+const connection = require('../config/database')
 const getHomePage = (req, res) =>{
+  connection.query(
+    `SELECT * FROM Users`,
+    function(err, results, fields) {
+      console.log(">>results: ", results);
+      console.log(">>field: ", fields);
+    }
+  )
   res.render('sample.ejs');
 }
 
--- src/server.js
+++ src/server.js
@@ -4,19 +4,13 @@ const path = require('path');
 require('dotenv').config();
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
-const mysql = require('mysql2');
+const connection = require('./config/database')
 const port = process.env.port || 3000;
 configViewEngine(app);
 app.use('/', webRoutes);
 
 // test connection
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3307,
-  user:  'root',
-  password: '123456',
-  database: 'vanh'
-});
+
 
 connection.query(
   `SELECT * FROM Users`,
